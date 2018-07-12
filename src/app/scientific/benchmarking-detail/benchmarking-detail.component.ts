ed78bfb62d1e18ade30f3bde0200bec190e53f31
Vicky Sundesha <me.vicky@hotmail.com>
2018-07-12 16:54:41 +0200

    chart update

--- src/app/scientific/benchmarking-detail/benchmarking-detail.component.ts
+++ src/app/scientific/benchmarking-detail/benchmarking-detail.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import * as chartb from '../shared/benchmarkingChart.js';
+import { ScientificService } from '../shared/scientific.service';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-benchmarking-detail',
@@ -6,10 +12,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./benchmarking-detail.component.css']
 })
 export class BenchmarkingDetailComponent implements OnInit {
-
-  constructor() { }
+  private id: string;
+  constructor(
+    private scientificService: ScientificService,
+    private route: ActivatedRoute,
+    private router: Router,
+  ) { }
 
   ngOnInit() {
+    this.id = this.getParam('bmid');
+    setTimeout(() => {
+      chartb.loadurl();
+    }, 300);
   }
 
+  click(): void {
+    setTimeout(() => {
+      chartb.onQuartileChange();
+    }, 300);
+
+  }
+  private getParam(param: string): string {
+    return this.route.snapshot.paramMap.get(param);
+  }
 }
