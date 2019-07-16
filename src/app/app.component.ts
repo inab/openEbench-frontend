4c64a5725987ed81a55c730527996ff70b890be5
Vicky Sundesha <me.vicky@hotmail.com>
2019-07-16 15:04:26 +0200

    google analytics added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
-
+declare var gtag;
 /**
  * main app component
  */
@@ -18,4 +20,14 @@ export class AppComponent {
    * title
    */
   title = 'app';
+  constructor(router: Router){
+    const navEndEvents = router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+    );
+    navEndEvents.subscribe((event: NavigationEnd) => {
+      gtag('config', 'UA-143782781-1', {
+        'page_path': event.urlAfterRedirects
+      });
+    });
+  }
 }
