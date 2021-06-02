7a0c04d63cb277a979c55ab9e693fc9bc064e917
Alejandro Asensio <alejandro.asensio.10@gmail.com>
2021-06-02 19:00:57 +0200

    refactor: graphql apollo client initialization following its official documentation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,10 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { KeycloakService, KeycloakAngularModule } from 'keycloak-angular';
 import { DataTablesModule } from 'angular-datatables';
-import { APOLLO_NAMED_OPTIONS, NamedOptions } from 'apollo-angular';
-import { HttpLink } from 'apollo-angular/http';
-import { InMemoryCache } from '@apollo/client/core';
 import { AppRoutingModule } from './app-routing.module';
 import { MaterialModule } from './material.module';
 import { initializer } from './utils/app-init';
 import { StatisticsService } from './shared/statistics.service';
-import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { TopMenuComponent } from './top-menu/top-menu.component';
 import { FooterComponent } from './footer/footer.component';
@@ -24,13 +20,6 @@ import { PrivateComponent } from './private/private.component';
 import { StatisticsComponent } from './statistics/statistics.component';
 import { GraphQLModule } from './graphql.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
-export function createApollo(httpLink: HttpLink) {
-  return {
-    link: httpLink.create({ uri: environment.SCIENTIFIC_SERVICE_URL }),
-    cache: new InMemoryCache(),
-  };
-}
 
 @NgModule({
   declarations: [
@@ -64,35 +53,7 @@ export function createApollo(httpLink: HttpLink) {
       useFactory: initializer,
       multi: true,
       deps: [KeycloakService],
-    },
-    {
-      provide: APOLLO_NAMED_OPTIONS,
-      useFactory(httpLink: HttpLink): NamedOptions {
-        return {
-          productionGraphqlClient: {
-            cache: new InMemoryCache(),
-            link: httpLink.create({
-              uri: 'https://openebench.bsc.es/sciapi/graphql',
-            }),
-          },
-        };
-      },
-      deps: [HttpLink],
-    },
-    {
-      provide: APOLLO_NAMED_OPTIONS,
-      useFactory(httpLink: HttpLink): NamedOptions {
-        return {
-          developmentGraphqlClient: {
-            cache: new InMemoryCache(),
-            link: httpLink.create({
-              uri: 'https://dev-openebench.bsc.es/sciapi/graphql',
-            }),
-          },
-        };
-      },
-      deps: [HttpLink],
-    },
+    }
   ],
   bootstrap: [AppComponent],
 })
--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -1,12 +1,14 @@
-import {NgModule} from '@angular/core';
-import {APOLLO_OPTIONS} from 'apollo-angular';
-import {ApolloClientOptions, InMemoryCache} from '@apollo/client/core';
-import {HttpLink} from 'apollo-angular/http';
+import { NgModule } from '@angular/core';
+import { APOLLO_OPTIONS } from 'apollo-angular';
+import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
+import { HttpLink } from 'apollo-angular/http';
+import { environment } from '../environments/environment';
 
-const uri = 'https://openebench.bsc.es/sciapi/graphql'; // <-- add the URL of the GraphQL server here
+const uri = environment.SCIENTIFIC_SERVICE_URL;
+
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   return {
-    link: httpLink.create({uri}),
+    link: httpLink.create({ uri }),
     cache: new InMemoryCache(),
   };
 }
