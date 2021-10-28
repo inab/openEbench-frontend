51e17c2b55318365e4025b13109d27fdaccca253
Dominik Brüchner <dominik.br@posteo.net>
2021-10-28 10:54:20 +0200

    Update environment.ts
    dev2 --> dev

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  ES_URL : 'https://dev2-openebench.bsc.es/esapi/filter',
+  ES_URL : 'https://dev-openebench.bsc.es/esapi/filter',
   AUTH_SERVER : 'https://inb.bsc.es/auth',
 
   TOOL_SEARCH_URL : 'https://dev-openebench.bsc.es/monitor/rest/edam/aggregate',
@@ -14,5 +14,5 @@ export const environment = {
   TOOL_STATISTICS_URL : 'https://dev-openebench.bsc.es/monitor/rest/statistics',
   METRICS_STATISTICS_URL : 'https://dev-openebench.bsc.es/monitor/rest/metrics/statistics',
 
-  SCIENTIFIC_SERVICE_URL : 'https://dev2-openebench.bsc.es/api/scientific/graphql'
+  SCIENTIFIC_SERVICE_URL : 'https://dev-openebench.bsc.es/api/scientific/graphql'
 };
