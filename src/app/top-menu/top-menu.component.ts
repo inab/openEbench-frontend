1d56905be2eb560508e964b1d63c0b06ab128823
Dominik Brüchner <dominik.br@posteo.net>
2021-11-05 10:12:07 +0100

    Revert "removed feedback form link from top-navigation bar"
    This reverts commit 18e826c278738c0105edf6692fbcb7c9b5f5a833.

--- src/app/top-menu/top-menu.component.ts
+++ src/app/top-menu/top-menu.component.ts
@@ -69,6 +69,11 @@ export class TopMenuComponent implements OnInit {
                 path: "",
                 href: "",
             },
+            {
+                label: "Feedback",
+                path: "",
+                href: "https://bsc3.typeform.com/to/wUKOEDiM",
+            },
         ];
         this.getProfileName();
 
