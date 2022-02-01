365f7026509c1d14fa2871683225a843869f68cd
Dominik Brüchner <dominik.br@posteo.net>
2022-02-01 18:55:47 +0100

    update prod env vars

--- src/environments/environment.prod.docker.ts
+++ src/environments/environment.prod.docker.ts
@@ -7,9 +7,9 @@
 export const environment = {
   production: true,
 
-  ES_URL : 'https://dev-openebench.bsc.es/esapi/filter',
+  ES_URL : 'https://openebench.bsc.es/esapi/filter',
   AUTH_SERVER : 'https://inb.bsc.es/auth',
-  TOOL_SEARCH_URL : 'https://dev-openebench.bsc.es/monitor/rest/edam/aggregate',
+  TOOL_SEARCH_URL : 'https://openebench.bsc.es/monitor/rest/edam/aggregate',
   TOOL_SERVICE_URL : 'https://openebench.bsc.es/monitor/rest/aggregate',
   TOOL_STATISTICS_URL : 'https://openebench.bsc.es/monitor/rest/statistics',
   METRICS_STATISTICS_URL : 'https://openebench.bsc.es/monitor/rest/metrics/statistics',
